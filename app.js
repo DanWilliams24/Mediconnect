72153f4aecd8481da6a4c5ab1ea4096c260b8fe3
DanWilliams24 <01danielwilliams@gmail.com>
2020-06-10 20:03:31 -0400

    Added .gitignore and installed Twilio API

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+//app.use('/help', users);
+//app.use('/info', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
